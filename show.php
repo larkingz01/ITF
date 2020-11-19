eeebc0c831b5e860ff921bc716a2b790b37dbe52
larkingz01 <73679235+larkingz01@users.noreply.github.com>
2020-11-19 21:04:07 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>˗ˏˋITF LABˎˊ˗</title>
+<title>itf krub</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -18,20 +18,20 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'aegis.mysql.database.azure.com', 'larking@aegis', 'ZXcvbnmoiu12', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL PLEASE TRY AGAIN !: '.mysqli_connect_error());
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 
 <div class="container">
     <div class="form">
-        <p class="ex1"><h1><center>꒰FORM =͟͟͞͞♡</center></h1></p>
+        <p class="ex1"><h1><center>Information</center></h1></p>
     </div>
-  <table width="600" border="1" class="table table-dark table-striped" class="center" class="ex1">
+  <table width="800" border="1" class="table table-dark table-striped" class="center" class="ex1">
     <tr class="info">
       <th width="200"> <div align="center">Name</div></th>
       <th width="350"> <div align="center">Comment </div></th>
-      <th width="50"> <div align="center">Action</div></th>
+      <th width="250"> <div align="center">Action</div></th>
     </tr>
   <?php
   while($Result = mysqli_fetch_array($res))
@@ -40,13 +40,13 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
     <tr>
       <td><center><?php echo $Result['Name'];?></center></td>
       <td><center><?php echo $Result['Comment'];?></center></td>
-      <td><center><a href="form edit.html"><input type="submit" value="EDIT" class="btn btn-warning"></a>&nbsp;&nbsp;<a href="form delete.html"><input type="submit" value="DELETE"  class="btn btn-danger"></a></center></td>
+      <td><center><a href="form edit.html"><input type="submit" value="Edit" class="btn btn-warning"></a>&nbsp;&nbsp;<a href="form delete.html"><input type="submit" value="Delete"  class="btn btn-danger"></a></center></td>
     </tr>
   <?php
   }
   ?>
   </table>
-  <center><a href="form insert.html"><input type="submit" value="INSERT" class="btn btn-info"></a></center>
+  <center><a href="form insert.html"><input type="submit" value="Add" class="btn btn-info"></a></center>
   <?php
   mysqli_close($conn);
   ?>
